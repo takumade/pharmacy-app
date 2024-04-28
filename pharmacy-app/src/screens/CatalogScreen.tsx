4982130841f2797c2d4a825032ca2763abcd3e4b
Richson <richonsimbabure@gmail.com>
2024-04-28 19:56:27 +0200

    chore: added a skeleton for the ui

---
+++ pharmacy-app/src/screens/CatalogScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const CatalogScreen = () => {
+  return (
+    <View>
+      <Text>CatalogScreen</Text>
+    </View>
+  )
+}
+
+export default CatalogScreen
+
+const styles = StyleSheet.create({})
