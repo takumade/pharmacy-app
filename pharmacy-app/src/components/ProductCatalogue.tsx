b620263b7dd808c3b4a1e0f776c7feb2cd6ce3a2
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-04-29 04:32:02 +0200

    Style em lists

--- pharmacy-app/src/components/ProductCatalogue.tsx
+++ pharmacy-app/src/components/ProductCatalogue.tsx
@@ -31,32 +31,53 @@ import HomeActionCards from "../components/HomeActionCards";
 
 import { List } from 'react-native-paper';
 
+
+const styles = StyleSheet.create({
+    productCatalogueContainer:  {
+    marginLeft: SPACING.space_30,
+    marginRight: SPACING.space_30,
+    marginTop: SPACING.space_30,
+    borderRadius: BORDERRADIUS.radius_20,
+    },
+    productCatalogueItem: {
+        backgroundColor: COLORS.primaryWhiteHex,
+        borderRadius: BORDERRADIUS.radius_20,
+        paddingLeft: SPACING.space_15,
+        marginTop: SPACING.space_10
+    }
+})
+
 const ProductCatalogue = () => {
     return (    <ScrollView>
-        <View>
+        <View style={styles.productCatalogueContainer}>
         <List.Item
       title="Medication"
       left={props => <List.Image  source={{uri: 'https://picsum.photos/700'}}/>}
+      style={styles.productCatalogueItem}
     />
   
   <List.Item
       title="Vitamins"
       left={props => <List.Image  source={{uri: 'https://picsum.photos/700'}}/>}
+      style={styles.productCatalogueItem}
     />
   
   <List.Item
       title="Suppliments"
       left={props => <List.Image  source={{uri: 'https://picsum.photos/700'}}/>}
+      style={styles.productCatalogueItem}
     />
   
   <List.Item
       title="Body & Care"
       left={props => <List.Image  source={{uri: 'https://picsum.photos/700'}}/>}
+      style={styles.productCatalogueItem}
     />
   
   <List.Item
       title="Products for children"
       left={props => <List.Image  source={{uri: 'https://picsum.photos/700'}}/>}
+      style={styles.productCatalogueItem}
     />
         </View>
   
