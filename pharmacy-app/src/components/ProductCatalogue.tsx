4ca7bd52be8cc9526d42f7271e494dd7d759da70
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-04-29 04:26:19 +0200

    Add product catalogue code

---
+++ pharmacy-app/src/components/ProductCatalogue.tsx
@@ -0,0 +1,66 @@
+
+
+import React from 'react'
+
+import {
+  StyleSheet, 
+  View,
+  ScrollView,
+  StatusBar,
+  Image
+} from 'react-native';
+import {ScreenContainer} from 'react-native-screens';
+import {
+  BORDERRADIUS,
+  COLORS,
+  FONTFAMILY,
+  FONTSIZE,
+  SPACING,
+} from '../theme/theme';
+import HeaderBar from '../components/HeaderBar';
+import CustomIcon from '../components/CustomIcon';
+
+import { TextInput } from 'react-native-paper';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+import { Icon, Card, IconButton, Button, Text } from 'react-native-paper';
+import FontAwesomeIcon from "react-native-vector-icons/FontAwesome5"
+import Products from '../components/Products';
+import ProductCategory from '../components/ProductCategory';
+import HomeActionCards from "../components/HomeActionCards";
+
+import { List } from 'react-native-paper';
+
+const ProductCatalogue = () => {
+    return (    <ScrollView>
+        <View>
+        <List.Item
+      title="Medication"
+      left={props => <List.Image  source={{uri: 'https://picsum.photos/700'}}/>}
+    />
+  
+  <List.Item
+      title="Vitamins"
+      left={props => <List.Image  source={{uri: 'https://picsum.photos/700'}}/>}
+    />
+  
+  <List.Item
+      title="Suppliments"
+      left={props => <List.Image  source={{uri: 'https://picsum.photos/700'}}/>}
+    />
+  
+  <List.Item
+      title="Body & Care"
+      left={props => <List.Image  source={{uri: 'https://picsum.photos/700'}}/>}
+    />
+  
+  <List.Item
+      title="Products for children"
+      left={props => <List.Image  source={{uri: 'https://picsum.photos/700'}}/>}
+    />
+        </View>
+  
+      </ScrollView>)
+}
+
+export default ProductCatalogue;
