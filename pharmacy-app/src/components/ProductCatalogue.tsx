a75aa03b097985e6a05460431eb5e4b8747a63b6
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-04-29 12:09:58 +0200

    Remove unused imports

--- pharmacy-app/src/components/ProductCatalogue.tsx
+++ pharmacy-app/src/components/ProductCatalogue.tsx
@@ -17,17 +17,6 @@ import {
   FONTSIZE,
   SPACING,
 } from '../theme/theme';
-import HeaderBar from '../components/HeaderBar';
-import CustomIcon from '../components/CustomIcon';
-
-import { TextInput } from 'react-native-paper';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-
-import { Icon, Card, IconButton, Button, Text } from 'react-native-paper';
-import FontAwesomeIcon from "react-native-vector-icons/FontAwesome5"
-import Products from '../components/Products';
-import ProductCategory from '../components/ProductCategory';
-import HomeActionCards from "../components/HomeActionCards";
 
 import { List } from 'react-native-paper';
 
@@ -52,6 +41,7 @@ const styles = StyleSheet.create({
 
 const ProductCatalogue = () => {
     return (    <View style={styles.screenContainer}>
+
         <ScrollView style={styles.productCatalogueContainer}>
         <List.Item
       title="Medication"
