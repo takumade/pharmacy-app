57134d8171b48e23917fe000474758de1bc018a9
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-04-30 15:08:58 +0200

    Add is deletedd field

--- server-api/src/controllers/userController.js
+++ server-api/src/controllers/userController.js
@@ -1,44 +1,73 @@
-const User = require("../models/userModel")
-
+const User = require("../models/userModel");
 
 const userRegister = async (req, res) => {
+  try {
+    const { username, email, phone, password } = req.body;
 
-    try{
-        const {username, email, phone, password} = req.body
+    let user = User.create({
+      username,
+      email,
+      phoneNumber: phone,
+      password,
+    });
 
+    delete user.password;
 
-            let user = User.create({
-                username,
-                email,
-                phoneNumber: phone,
-                password
-            })
+    res.send({
+      success: true,
+      message: "User Created",
+      data: user,
+    });
+  } catch (err) {
+    res.send({
+      success: false,
+      message: "Could not create user",
+      data: err,
+    });
+  }
+};
 
-            delete user.password
+const userLogin = async (req, res) => {
+  const { email, password } = req.body;
 
-            res.send({
-                success: true, 
-                message: 'User Created',
-                data: user
-            })
+  try {
+    // Check if user exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid email or password" });
+    }
 
-        }catch(err){
-            res.send({
-                success: false,
-                message: 'Could not create user',
-                data: err
-            })
-        }
+    // Check if password is correct
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid email or password" });
+    }
 
-}
+    // Check if user is verified
+    if (!user.isVerified) {
+      return res
+        .status(400)
+        .json({
+          success: false,
+          message: "Please verify your email before logging in",
+        });
+    }
 
-const userLogin = (req, res) => {
+    // Generate auth token
+    const token = user.generateAuthToken();
 
-}
-
-
+    res.status(200).json({ success: true, data: token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+};
 
 module.exports = {
-    userRegister,
-    userLogin
-}
+  userRegister,
+  userLogin,
+};
