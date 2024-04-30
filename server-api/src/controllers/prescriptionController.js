03ff05f16bdf4ae30b90111ffbe53eab2b2cb06e
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-04-30 17:16:01 +0200

    Can delete prescrioption

--- server-api/src/controllers/prescriptionController.js
+++ server-api/src/controllers/prescriptionController.js
@@ -1,4 +1,5 @@
-const Pharmacy = require("../models/pharmacyModel")
+const Pharmacy = require("../models/pharmacyModel");
+const Prescription = require("../models/prescriptionModel");
 
 
 
@@ -50,6 +51,34 @@ const approvePrescription = async (req, res) => {
 
         // Send success response
         res.status(200).json({ success: true, message: "Prescription approved successfully", prescription });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: "Internal server error" });
+    }
+};
+
+
+const deletePrescription = async (req, res) => {
+    try {
+        // Extract prescription ID from the request parameters
+        const prescriptionId = req.params.prescriptionId;
+
+        // Find the prescription in the database
+        const prescription = await Prescription.findById(prescriptionId);
+        if (!prescription) {
+            return res.status(404).json({ success: false, message: "Prescription not found" });
+        }
+
+        // Check if the user making the request is the owner of the prescription or an admin
+        if ((req.user.role !== 'admin' || req.user.role !== 'pharmacy') && String(prescription.owner) !== String(req.user._id)) {
+            return res.status(403).json({ success: false, message: "You are not authorized to delete this prescription" });
+        }
+
+        // Delete the prescription from the database
+        await Prescription.findByIdAndDelete(prescriptionId);
+
+        // Send success response
+        res.status(200).json({ success: true, message: "Prescription deleted successfully" });
     } catch (error) {
         console.error(error);
         res.status(500).json({ success: false, message: "Internal server error" });
