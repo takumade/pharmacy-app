692b689f60e1823d7a8c8bc9edbdd46b4783b78a
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-04-30 16:10:09 +0200

    Register routes

--- server-api/src/index.js
+++ server-api/src/index.js
@@ -9,7 +9,12 @@ app.use(cors());
 app.use(express.json());
 
 // Define your routes here
+const userRoutes = require("./routes/userRoutes");
+const pharmacyRoutes = require('./routes/phamarcyRoutes');
 const medicineRoutes = require('./routes/medicineRoutes');
+
+app.use('/api/user', userRoutes);
+app.use('/pharmacy', pharmacyRoutes)
 app.use('/api/medicine', medicineRoutes);
 
 // Start the server
