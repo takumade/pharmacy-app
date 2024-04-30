a4c006176ce4674706a895701650c271aba54ca1
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-04-30 17:00:51 +0200

    Add medicine routes

--- server-api/src/routes/medicineRoutes.js
+++ server-api/src/routes/medicineRoutes.js
@@ -3,7 +3,14 @@ const router = express.Router();
 const medicineController = require('../controllers/medicineController');
 
 // Define your routes
-router.get('/', medicineController.searchMedicines);
+router.get('/', medicineController.getMedicines);
+router.get('/:medicineId', medicineController.getMedicine);
+
+router.post('/create', medicineController.addMedicine);
+router.post('/create-many', medicineController.addMedicines);
+router.post('/edit/:medicineId', medicineController.editMedicine);
+router.post('/delete/:medicineId', medicineController.deleteMedicine);
+router.post('/delete-many', medicineController.deleteMedicines);
 // Add more routes as needed
 
 module.exports = router;
