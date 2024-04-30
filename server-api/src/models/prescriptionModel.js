24ec05317e752031b29dc4e513a35a36b2f6110f
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-04-30 17:07:41 +0200

    Add more fields

--- server-api/src/models/prescriptionModel.js
+++ server-api/src/models/prescriptionModel.js
@@ -11,6 +11,14 @@ const prescriptionSchema = new Schema({
         type: String,
         required: true
     },
+    approved: {
+        type: Boolean,
+        default: false
+    },
+    used: {
+        type: Boolean,
+        default: false
+    },
     date: {
         type: Date,
         default: Date.now
