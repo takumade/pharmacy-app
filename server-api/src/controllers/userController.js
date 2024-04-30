02db4517b07f5bbc3855db0433c319fee5d8f864
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-04-30 15:11:39 +0200

    Delete user account

--- server-api/src/controllers/userController.js
+++ server-api/src/controllers/userController.js
@@ -67,6 +67,28 @@ const userLogin = async (req, res) => {
   }
 };
 
+const deleteAccount = async (req, res) => {
+    const userId = req.user._id;
+  
+    try {
+      // Find the user by ID
+      const user = await User.findById(userId);
+      if (!user) {
+        return res.status(404).json({ success: false, message: "User not found" });
+      }
+  
+      // Soft delete the user account
+      user.isDeleted = true;
+      await user.save();
+  
+      res.status(200).json({ success: true, message: "Account deleted successfully" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ success: false, message: "Internal server error" });
+    }
+  }
+  
+
 module.exports = {
   userRegister,
   userLogin,
