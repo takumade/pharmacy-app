36988d1a1c4e534c57534f833e43eb7ecdfadf1b
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-04-30 17:24:06 +0200

    Get many prescriptions

--- server-api/src/controllers/prescriptionController.js
+++ server-api/src/controllers/prescriptionController.js
@@ -2,6 +2,55 @@ const Pharmacy = require("../models/pharmacyModel");
 const Prescription = require("../models/prescriptionModel");
 
 
+const getPrescription = async (req, res) => {
+    try {
+        // Extract prescription ID from the request parameters
+        const prescriptionId = req.params.prescriptionId;
+
+        // Find the prescription in the database
+        const prescription = await Prescription.findById(prescriptionId);
+        if (!prescription) {
+            return res.status(404).json({ success: false, message: "Prescription not found" });
+        }
+
+        // Check if the user making the request is the owner of the prescription, a pharmacy, or an admin
+        if (req.user.role !== 'admin' && String(prescription.owner) !== String(req.user._id) && req.user.role !== 'pharmacy') {
+            return res.status(403).json({ success: false, message: "You are not authorized to access this prescription" });
+        }
+
+        // Send the prescription in the response
+        res.status(200).json({ success: true, prescription });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: "Internal server error" });
+    }
+}
+
+const getPrescriptions = async (req, res) => {
+    try {
+        let prescriptions;
+
+        // Check if the user making the request is a pharmacy or an admin
+        if (req.user.role === 'admin') {
+            // If the user is a pharmacy or an admin, retrieve all prescriptions
+            prescriptions = await Prescription.find();
+        } else if (req.user.role === 'customer') {
+            // If the user is a customer, retrieve only their own prescriptions
+            prescriptions = await Prescription.find({ owner: req.user._id });
+        }else if(req.user.role === 'pharmacy'){
+            // TODO: get prescriotions submited to a pharmarcy 
+        } else {
+            // If the user role is not recognized, return a 403 Forbidden response
+            return res.status(403).json({ success: false, message: "You are not authorized to access prescriptions" });
+        }
+
+        // Send the prescriptions in the response
+        res.status(200).json({ success: true, prescriptions });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: "Internal server error" });
+    }
+};
 
 const createPrescription = async (req, res) => {
     try {
@@ -86,6 +135,8 @@ const deletePrescription = async (req, res) => {
 };
 
 module.exports = {
+    getPrescription,
+    getPrescriptions,
     createPrescription,
     approvePrescription,
     deletePrescription
