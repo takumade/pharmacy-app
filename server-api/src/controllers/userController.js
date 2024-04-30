d77c47da4e4c2f441b99d20f63f2d0e5e47745f0
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-04-30 16:20:00 +0200

    Add getUsers

--- server-api/src/controllers/userController.js
+++ server-api/src/controllers/userController.js
@@ -1,29 +1,57 @@
 const User = require("../models/userModel");
 
-
 const getUser = async (req, res) => {
-    const currentUser = req.user;
-    const userId = req.params.userId;
-  
-    try {
-      // Check if the user making the request is an admin
-      if (currentUser.role !== 'admin' && String(currentUser._id) !== userId) {
-        return res.status(403).json({ success: false, message: "You are not authorized to access this user's information" });
-      }
-  
-      // Find the user by ID
-      const user = await User.findById(userId);
-      if (!user) {
-        return res.status(404).json({ success: false, message: "User not found" });
-      }
-  
-      res.status(200).json({ success: true, user });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ success: false, message: "Internal server error" });
+  const currentUser = req.user;
+  const userId = req.params.userId;
+
+  try {
+    // Check if the user making the request is an admin
+    if (currentUser.role !== "admin" && String(currentUser._id) !== userId) {
+      return res
+        .status(403)
+        .json({
+          success: false,
+          message: "You are not authorized to access this user's information",
+        });
     }
+
+    // Find the user by ID
+    const user = await User.findById(userId);
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+
+    res.status(200).json({ success: true, user });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: "Internal server error" });
   }
-  
+};
+
+const getUsers = async (req, res) => {
+  const currentUser = req.user;
+
+  try {
+    // Check if the user making the request is an admin
+    if (currentUser.role !== "admin") {
+      return res
+        .status(403)
+        .json({
+          success: false,
+          message: "You are not authorized to access user information",
+        });
+    }
+
+    // Find all users
+    const users = await User.find();
+    res.status(200).json({ success: true, users });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+};
 
 const userRegister = async (req, res) => {
   try {
@@ -74,12 +102,10 @@ const userLogin = async (req, res) => {
 
     // Check if user is verified
     if (!user.isVerified) {
-      return res
-        .status(400)
-        .json({
-          success: false,
-          message: "Please verify your email before logging in",
-        });
+      return res.status(400).json({
+        success: false,
+        message: "Please verify your email before logging in",
+      });
     }
 
     // Generate auth token
@@ -93,51 +119,76 @@ const userLogin = async (req, res) => {
 };
 
 const deleteAccount = async (req, res) => {
-    const userId = req.params.userId;
-    const currentUser = req.user;
-  
-    try {
-      // Find the user making the request
-      const userMakingRequest = await User.findById(currentUser._id);
-      if (!userMakingRequest) {
-        return res.status(404).json({ success: false, message: "User not found" });
-      }
-  
-      // If the user is not an admin and is trying to delete another user's account, deny access
-      if (userMakingRequest.role !== 'admin' && String(userMakingRequest._id) !== userId) {
-        return res.status(403).json({ success: false, message: "You are not authorized to delete this account" });
-      }
-  
-      // Find the user to be deleted
-      const userToDelete = await User.findById(userId);
-      if (!userToDelete) {
-        return res.status(404).json({ success: false, message: "User not found" });
-      }
-  
-      // Admin can delete any user's account
-      if (userMakingRequest.role === 'admin') {
-        userToDelete.isDeleted = true;
-        await userToDelete.save();
-        return res.status(200).json({ success: true, message: "Account deleted successfully" });
-      }
-  
-      // Non-admin user can only delete their own account
-      if (String(userToDelete._id) === String(userMakingRequest._id)) {
-        userToDelete.isDeleted = true;
-        await userToDelete.save();
-        return res.status(200).json({ success: true, message: "Account deleted successfully" });
-      }
-  
-      // If none of the conditions match, return unauthorized
-      return res.status(403).json({ success: false, message: "You are not authorized to delete this account" });
-    } catch (error) {
-      console.error(error);
-      return res.status(500).json({ success: false, message: "Internal server error" });
+  const userId = req.params.userId;
+  const currentUser = req.user;
+
+  try {
+    // Find the user making the request
+    const userMakingRequest = await User.findById(currentUser._id);
+    if (!userMakingRequest) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
     }
+
+    // If the user is not an admin and is trying to delete another user's account, deny access
+    if (
+      userMakingRequest.role !== "admin" &&
+      String(userMakingRequest._id) !== userId
+    ) {
+      return res
+        .status(403)
+        .json({
+          success: false,
+          message: "You are not authorized to delete this account",
+        });
+    }
+
+    // Find the user to be deleted
+    const userToDelete = await User.findById(userId);
+    if (!userToDelete) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+
+    // Admin can delete any user's account
+    if (userMakingRequest.role === "admin") {
+      userToDelete.isDeleted = true;
+      await userToDelete.save();
+      return res
+        .status(200)
+        .json({ success: true, message: "Account deleted successfully" });
+    }
+
+    // Non-admin user can only delete their own account
+    if (String(userToDelete._id) === String(userMakingRequest._id)) {
+      userToDelete.isDeleted = true;
+      await userToDelete.save();
+      return res
+        .status(200)
+        .json({ success: true, message: "Account deleted successfully" });
+    }
+
+    // If none of the conditions match, return unauthorized
+    return res
+      .status(403)
+      .json({
+        success: false,
+        message: "You are not authorized to delete this account",
+      });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal server error" });
   }
+};
 
 module.exports = {
+  getUser,
+  getUsers,
   userRegister,
   userLogin,
-  deleteAccount
+  deleteAccount,
 };
