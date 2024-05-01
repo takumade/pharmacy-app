f23adb12cf6d17a3b4485a829b5cd0be19ba73d1
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-05-01 16:54:17 +0200

    get order route

---
+++ server-api/src/routes/ordersRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const authMiddleware= require('../middlewares/authMiddleware');
+const orderController = require('../controllers/orderController');
+
+
+router.use(authMiddleware.authenticateUser);
+
+// Define your routes
+router.get('/', orderController.getOrders);
+router.get('/:medicineId', medicineController.getMedicine);
+
+router.post('/create', medicineController.addMedicine);
+router.post('/create-many', medicineController.addMedicines);
+router.post('/edit/:medicineId', medicineController.editMedicine);
+router.post('/delete/:medicineId', medicineController.deleteMedicine);
+router.post('/delete-many', medicineController.deleteMedicines);
+// Add more routes as needed
+
+module.exports = router;
