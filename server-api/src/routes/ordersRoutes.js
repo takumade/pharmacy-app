78247d43bb2cb87735ca23a9a8967b0c93a595d4
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-05-01 16:56:26 +0200

    Remove typo

--- server-api/src/routes/ordersRoutes.js
+++ server-api/src/routes/ordersRoutes.js
@@ -9,11 +9,10 @@ router.use(authMiddleware.authenticateUser);
 // Define your routes
 router.get('/', orderController.getOrders);
 router.get('/:pharmacyId', orderController.getOrders);
-router.get('/:orderId', orderController.getOrders);
+router.get('/:orderId', orderController.getOrder);
 
 router.post('/create', orderController.checkout);
 router.post('/delete/:orderId', orderController.deleteOrder);
-router.post('/delete-many', medicineController.deleteMedicines);
 // Add more routes as needed
 
 module.exports = router;
