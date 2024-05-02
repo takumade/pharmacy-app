f7e71ee10e1caf3b7663768cc3b8ecfcac05b5c2
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-05-02 17:56:00 +0200

    Get transaction routes

---
+++ server-api/src/routes/transactionRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const transactionController = require('../controllers/transactionController');
+const authMiddleware= require('../middlewares/authMiddleware');
+
+
+router.use(authMiddleware.authenticateUser);
+
+// Define your routes
+router.get('/', transactionController.getTransactions);
+router.get('/:medicineId', medicineController.getMedicine);
+
+router.post('/create', medicineController.addMedicine);
+router.post('/create-many', medicineController.addMedicines);
+router.post('/edit/:medicineId', medicineController.editMedicine);
+router.post('/delete/:medicineId', medicineController.deleteMedicine);
+router.post('/delete-many', medicineController.deleteMedicines);
+// Add more routes as needed
+
+module.exports = router;
