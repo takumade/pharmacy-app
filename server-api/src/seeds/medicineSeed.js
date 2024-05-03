7445407df0bbe9998583b41b9ee26360c3732472
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-05-03 13:42:08 +0200

    Remove depreacted images

--- server-api/src/seeds/medicineSeed.js
+++ server-api/src/seeds/medicineSeed.js
@@ -3,20 +3,20 @@ const { faker } = require("@faker-js/faker");
 // Generate seed object for medicine
 const generateMedicineSeed = (pharmacyId=22) => {
   return {
-    medicineName: faker.random.words(),
-    image: faker.image.imageUrl(),
+    medicineName: faker.word.words(),
+    image: faker.image.url(),
     brandName: faker.company.name(),
-    genericName: faker.random.words(),
-    dosageForm: faker.random.words(),
-    dosageStrength: faker.random.words(),
-    batchNumber: faker.random.alphaNumeric(10),
+    genericName: faker.word.words(),
+    dosageForm: faker.word.words(),
+    dosageStrength: faker.word.words(),
+    batchNumber: faker.string.alphanumeric(10),
     expirationDate: faker.date.future(),
-    quantity: faker.datatype.number(),
-    unitPrice: faker.datatype.number(),
-    totalPrice: faker.datatype.number(),
+    quantity: faker.number.int(),
+    unitPrice: faker.number.float(),
+    totalPrice: faker.number.float(),
     manufacturer: faker.company.name(),
     supplier: faker.company.name(),
-    storageConditions: faker.random.words(),
+    storageConditions: faker.word.words(),
     notes: faker.lorem.paragraph(),
     lastUpdated: faker.date.recent(),
     prescriptionRequired: faker.datatype.boolean(),
@@ -24,6 +24,5 @@ const generateMedicineSeed = (pharmacyId=22) => {
   };
 };
 
-// Example usage:
-const medicineSeed = generateMedicineSeed();
-console.log(medicineSeed);
+module.exports = generateMedicineSeed
+
