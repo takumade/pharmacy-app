34e06f4bfd08721a302d4a9372d2f9239910a333
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-05-04 17:54:53 +0200

    Chnage the way data is generated

--- server-api/src/seeds/medicineSeed.js
+++ server-api/src/seeds/medicineSeed.js
@@ -2,18 +2,17 @@ const { faker } = require("@faker-js/faker");
 
 // Generate seed object for medicine
 const medicineSeed = (pharmacyId=22, count = 1) => {
-  return Array.from(Array(count)).map(d => ({
-    medicineName: faker.word.words(),
+  let medicine = Array.from(Array(count)).map(d => ({
+    medicineName: faker.commerce.productName(),
     image: faker.image.url(),
     brandName: faker.company.name(),
-    genericName: faker.word.words(),
+    genericName: faker.commerce.productAdjective(),
     dosageForm: faker.word.words(),
     dosageStrength: faker.word.words(),
     batchNumber: faker.string.alphanumeric(10),
     expirationDate: faker.date.future(),
-    quantity: faker.number.int(),
-    unitPrice: faker.number.float(),
-    totalPrice: faker.number.float(),
+    quantity: faker.number.int({min: 10, max: 100}),
+    unitPrice: faker.commerce.price({min: 2}),
     manufacturer: faker.company.name(),
     supplier: faker.company.name(),
     storageConditions: faker.word.words(),
@@ -22,6 +21,10 @@ const medicineSeed = (pharmacyId=22, count = 1) => {
     prescriptionRequired: faker.datatype.boolean(),
     owner: pharmacyId // You need to replace this with the appropriate owner value
   }))
+
+  return medicine
+
+
 };
 
 module.exports = medicineSeed
