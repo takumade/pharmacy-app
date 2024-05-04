cb87dfcfc39df9913522c8862cf3e6a2df238082
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-05-04 18:47:30 +0200

    Add connect db in index

--- server-api/src/index.js
+++ server-api/src/index.js
@@ -3,10 +3,8 @@ const config = require('./config/config');
 
 // Start the server
 app.listen(config.port, () => {
-  console.log(`Server is running on port ${config.port}`);
+  console.log(`[+] Server is running on port ${config.port}`);
 
-  console.log('config: ', config);
-  
-
+  connectDB()
   
 });
