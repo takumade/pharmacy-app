98b1a8fd405d58a84378c83ac62cd73d8aa96efd
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-05-05 00:40:08 +0200

    Return result as data

--- server-api/src/controllers/pharmacyController.js
+++ server-api/src/controllers/pharmacyController.js
@@ -25,7 +25,7 @@ const getPharmacy = async (req, res) => {
     try {
       // Find all pharmacies
       const pharmacies = await Pharmacy.find();
-      res.status(200).json({ success: true, pharmacies });
+      res.status(200).json({ success: true, data: pharmacies });
     } catch (error) {
       console.error(error);
       res.status(500).json({ success: false, message: "Internal server error" });
@@ -47,7 +47,7 @@ const createPharmacy = async (req, res) => {
       // Save the new pharmacy to the database
       await newPharmacy.save();
   
-      res.status(201).json({ success: true, message: "Pharmacy created successfully", pharmacy: newPharmacy });
+      res.status(201).json({ success: true, message: "Pharmacy created successfully", data: newPharmacy });
     } catch (error) {
       console.error(error);
       res.status(500).json({ success: false, message: "Internal server error" });
@@ -82,7 +82,7 @@ const createPharmacy = async (req, res) => {
       // Save the updated pharmacy
       await pharmacy.save();
   
-      res.status(200).json({ success: true, message: "Pharmacy updated successfully", pharmacy });
+      res.status(200).json({ success: true, message: "Pharmacy updated successfully", data: pharmacy });
     } catch (error) {
       console.error(error);
       res.status(500).json({ success: false, message: "Internal server error" });
@@ -138,7 +138,7 @@ const approvePharmacy = async (req, res) => {
       pharmacy.isApproved = true;
       await pharmacy.save();
   
-      res.status(200).json({ success: true, message: "Pharmacy approved successfully", pharmacy });
+      res.status(200).json({ success: true, message: "Pharmacy approved successfully", data: pharmacy });
     } catch (error) {
       console.error(error);
       res.status(500).json({ success: false, message: "Internal server error" });
@@ -173,7 +173,7 @@ const approvePharmacy = async (req, res) => {
         const customers = await User.find({ _id: { $in: userIds } });
 
         // Send the list of customers in the response
-        res.status(200).json({ success: true, customers });
+        res.status(200).json({ success: true, data: customers });
     } catch (error) {
         console.error(error);
         res.status(500).json({ success: false, message: "Internal server error" });
