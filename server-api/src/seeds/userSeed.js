bd2a20b2e4e7f8673c2adf172a68cdcecbcf47bb
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-05-05 00:18:33 +0200

    Saved hashed password and cleartext password

--- server-api/src/seeds/userSeed.js
+++ server-api/src/seeds/userSeed.js
@@ -1,81 +1,88 @@
 const { faker } = require("@faker-js/faker");
 
 const generateCustomers = (customersNo = 10) => {
-let customers = [];
+  let customers = [];
 
-for (let index = 0; index < customersNo; index++) {
-  customers.push({
-    username: faker.internet.userName(),
-    fullName: faker.person.fullName(),
-    email: faker.internet.email(),
-    phoneNumber: faker.phone.number(),
-    password: "password123",
-    role: "customer",
-    isVerified: true,
-    avatar: faker.image.url()
-  });
-}
+  for (let index = 0; index < customersNo; index++) {
+    customers.push({
+      username: faker.internet.userName(),
+      fullName: faker.person.fullName(),
+      email: faker.internet.email(),
+      phoneNumber: faker.phone.number(),
+      password: "password123",
+      role: "customer",
+      isVerified: true,
+      avatar: faker.image.url(),
+    });
+  }
 
-return customers
-
-}
+  return customers;
+};
 
 const generartePharmacies = (pharmacyNo = 10) => {
- 
-let pharmacies = [];
+  let pharmacies = [];
 
-for (let index = 0; index < pharmacyNo; index++) {
-  pharmacies.push({
-    username: faker.internet.userName(), 
-    fullName: faker.person.fullName(),
-    email: faker.internet.email(),
-    phoneNumber: faker.phone.number(),
-    password: "password123",
-    role: "pharmacy",
-    isVerified: true,
-    avatar: faker.image.url(),
-    verificationImage: faker.image.url()
-  });
-}
+  for (let index = 0; index < pharmacyNo; index++) {
+    pharmacies.push({
+      username: faker.internet.userName(),
+      fullName: faker.person.fullName(),
+      email: faker.internet.email(),
+      phoneNumber: faker.phone.number(),
+      password: "password123",
+      role: "pharmacy",
+      isVerified: true,
+      avatar: faker.image.url(),
+      verificationImage: faker.image.url(),
+    });
+  }
 
-return pharmacies
-
-}
+  return pharmacies;
+};
 
 const generateAdmins = () => {
-    let admins = [
-        {
-            username: "admin1",
-            fullName: "Takunda Made",
-            email: "admin1@example.com",
-            phoneNumber: faker.phone.number(),
-            password: "password123",
-            role: "admin",
-            isVerified: true,
-            avatar: faker.image.url()
-          },
-          {
-            username: "admin2",
-            fullName: "Richson Simba",
-            email: "admin2@example.com",
-            phoneNumber: faker.phone.number(),
-            password: "password123",
-            role: "admin",
-            isVerified: true,
-            avatar: faker.image.url()
-          }
-    ]
+  let admins = [
+    {
+      username: "admin1",
+      fullName: "Takunda Made",
+      email: "admin1@example.com",
+      phoneNumber: faker.phone.number(),
+      password: "password123",
+      role: "admin",
+      isVerified: true,
+      avatar: faker.image.url(),
+    },
+    {
+      username: "admin2",
+      fullName: "Richson Simba",
+      email: "admin2@example.com",
+      phoneNumber: faker.phone.number(),
+      password: "password123",
+      role: "admin",
+      isVerified: true,
+      avatar: faker.image.url(),
+    },
+  ];
 
-    return admins
-    
-}
+  return admins;
+};
 
-const userSeed = () => {
-    return [
-        ...generartePharmacies(),
-        ...generateCustomers(),
-        ...generateAdmins()
-    ]
-}
+const hashPass = async (pass) => {
+  const salt = await bcrypt.genSalt(10);
+  return await bcrypt.hash(pass, salt);
+};
 
-module.exports = userSeed
+const userSeed = async () => {
+  let allUsers = [
+    ...generartePharmacies(),
+    ...generateCustomers(),
+    ...generateAdmins(),
+  ].map(async (user) => ({
+    ...user,
+    password: await hashPass(password),
+    clearText: password,
+  }));
+
+  return allUsers;
+};
+
+module.exports = userSeed;
