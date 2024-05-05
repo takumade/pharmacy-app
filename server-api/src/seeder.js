057ab8094937eef63c509f9185601242f18f1f32
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-05-05 23:49:29 +0200

    Seed realistic pharmacies

--- server-api/src/seeder.js
+++ server-api/src/seeder.js
@@ -14,54 +14,66 @@ const Order = require("./models/orderModel");
 const transactionSeed = require("./seeds/transactionSeed");
 const Transaction = require("./models/transactionModel");
 
-
 const seedData = async () => {
   // Seed users first
-  console.log("[+] Seeding users....")
-  let users = await User.insertMany(await userSeed())
+  console.log("[+] Seeding users....");
+  let users = await User.insertMany(await userSeed());
+
+  let customers = users.filter((user) => user.role === "customer");
+  let pharmacyOwners = users.filter((user) => user.role === "pharmacy");
+
+  console.log("[+] Seeding presciptions....");
+  let prescriptions = await Prescription.insertMany(
+    customers.map((customer) => prescriptionSeed(customer._id))
+  );
+
+  console.log("[+] Seeding pharmacies....");
+  let generatedPharmacies = pharmacyOwners.map((pharmacy, index) =>
+    pharmacySeed(index, pharmacy._id)
+  );
   
-  let customers = users.filter(user => user.role === "customer")
-  let pharmacies = users.filter(user => user.role === "pharmacy")
+  let pharmaciesList = await Pharmacy.insertMany(generatedPharmacies);
 
-  console.log("[+] Seeding presciptions....")
-  let prescriptions = await Prescription.insertMany(customers.map(customer => prescriptionSeed(customer._id)))
+  // Grap  pharmacies and seed medicine
+  console.log("[+] Seeding medicines, orders and transactions....");
 
-  console.log("[+] Seeding pharmacies....")
-  let pharmaciesList = await Pharmacy.insertMany(pharmacies.map(pharmacy => pharmacySeed(pharmacy._id)))
-
-  let insertMedicines = []
-
-
-   // Grap  pharmacies and seed medicine
-   console.log("[+] Seeding medicines, orders and transactions....")
-
-   for (let index = 0; index < pharmaciesList.length; index++) {
+  for (let index = 0; index < pharmaciesList.length; index++) {
     const pharmacy = pharmaciesList[index];
 
     // Seed medicines
-    let medicines  = await Medicine.insertMany(medicineSeed(pharmacy._id, 10))
+    let medicines = await Medicine.insertMany(medicineSeed(pharmacy._id, 10));
 
-  customers.forEach(async(customer) => {
-      let products = medicines.slice(0, 3).map(medicine => ({
+    customers.forEach(async (customer) => {
+      let products = medicines.slice(0, 3).map((medicine) => ({
         productId: medicine._id,
         productName: medicine.medicineName,
         quantity: 2,
-        price: medicine.unitPrice
+        price: medicine.unitPrice,
       }));
 
-      let userPrescriptions = prescriptions.filter(p => p.owner == customer._id)
+      let userPrescriptions = prescriptions.filter(
+        (p) => p.owner == customer._id
+      );
 
-      let order = await orderSeed(customer._id, pharmacy._id, products,  userPrescriptions[0])
-      let createdOrder = await Order.create(order)
+      let order = await orderSeed(
+        customer._id,
+        pharmacy._id,
+        products,
+        userPrescriptions[0]
+      );
+      let createdOrder = await Order.create(order);
 
-      let txn = await transactionSeed(customer._id, pharmacy._id, createdOrder._id, createdOrder.totalAmount)
-      let createdTxn = await Transaction.create(txn)
+      let txn = await transactionSeed(
+        customer._id,
+        pharmacy._id,
+        createdOrder._id,
+        createdOrder.totalAmount
+      );
+      let createdTxn = await Transaction.create(txn);
+    });
+  }
 
-    }) 
-   }
-
- 
-  console.log("[100%] Done sedding!....")
+  console.log("[100%] Done sedding!....");
   process.exit(0);
 };
 
