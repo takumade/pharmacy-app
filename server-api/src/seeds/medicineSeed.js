652cc8820a309533df115b8d48f2cf1f38ab3251
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-05-05 21:59:55 +0200

    Add some realistic data to the seed

--- server-api/src/seeds/medicineSeed.js
+++ server-api/src/seeds/medicineSeed.js
@@ -1,24 +1,26 @@
 const { faker } = require("@faker-js/faker");
+const medicineData = require("./medicineData");
+
 
 // Generate seed object for medicine
 const medicineSeed = (pharmacyId=22, count = 1) => {
-  let medicine = Array.from(Array(count)).map(d => ({
-    medicineName: faker.commerce.productName(),
+  let medicine = medicineData.slice(0, count).map(m => ({
+    medicineName: m.medicineName,
     image: faker.image.url(),
-    brandName: faker.company.name(),
-    genericName: faker.commerce.productAdjective(),
-    dosageForm: faker.word.words(),
-    dosageStrength: faker.word.words(),
+    brandName: m.brandName,
+    genericName: m.genericName,
+    dosageForm: m.dosageForm,
+    dosageStrength: m.dosageStrength,
     batchNumber: faker.string.alphanumeric(10),
     expirationDate: faker.date.future(),
     quantity: faker.number.int({min: 10, max: 100}),
-    unitPrice: faker.commerce.price({min: 2}),
-    manufacturer: faker.company.name(),
+    unitPrice: m.price,
+    manufacturer: m.manufacturer,
     supplier: faker.company.name(),
     storageConditions: faker.word.words(),
     notes: faker.lorem.paragraph(),
     lastUpdated: faker.date.recent(),
-    prescriptionRequired: faker.datatype.boolean(),
+    prescriptionRequired: m.prescriptionRequired,
     owner: pharmacyId // You need to replace this with the appropriate owner value
   }))
 
