3072216265b393489fd7ba3ecb0659ffa74521bb
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-05-06 16:29:31 +0200

    Add nav items

--- dashboard/src/components/dashboard/layout/config.ts
+++ dashboard/src/components/dashboard/layout/config.ts
@@ -4,7 +4,11 @@ import { paths } from '@/paths';
 export const navItems = [
   { key: 'overview', title: 'Overview', href: paths.dashboard.overview, icon: 'chart-pie' },
   { key: 'customers', title: 'Customers', href: paths.dashboard.customers, icon: 'users' },
+  { key: 'medicine', title: 'Medicine', href: paths.dashboard.medicine, icon: 'pill' },
+  { key: 'prescriptions', title: 'Prescriptions', href: paths.dashboard.medicine, icon: 'scroll' },
   { key: 'integrations', title: 'Integrations', href: paths.dashboard.integrations, icon: 'plugs-connected' },
+  { key: 'orders', title: 'Orders', href: paths.dashboard.medicine, icon: 'shopping-bag' },
+  { key: 'transactions', title: 'Transactions', href: paths.dashboard.medicine, icon: 'money' },
   { key: 'settings', title: 'Settings', href: paths.dashboard.settings, icon: 'gear-six' },
   { key: 'account', title: 'Account', href: paths.dashboard.account, icon: 'user' },
   { key: 'error', title: 'Error', href: paths.errors.notFound, icon: 'x-square' },
