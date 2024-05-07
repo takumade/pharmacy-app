21f90ebc399b2348185a30d726b9c273e0dfa438
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-05-07 14:23:58 +0200

    Add authorization to client

--- dashboard/src/services/client.ts
+++ dashboard/src/services/client.ts
@@ -1,6 +1,14 @@
-const fetchData = async (url:string, options: any) => {
+const fetchData = async (method:string, path:string, body: any) => {
   try {
-    const response = await fetch("", options);
+    const response = await fetch(`${process.env.BACKEND_API}/api/${path}`, {
+      method: method,
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': localStorage.getItem('custom-auth-token')
+      },
+      body: JSON.stringify(body)
+
+    });
     const data = await response.json();
     return data;
   } catch (error) {
