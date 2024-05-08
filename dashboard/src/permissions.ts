2bb4f733f513cd359486d91d1fbdf69aec140e44
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-05-08 16:14:45 +0200

    getPermisssion gittem role and page

--- dashboard/src/permissions.ts
+++ dashboard/src/permissions.ts
@@ -12,3 +12,28 @@ export const medicinePerms: RolePerm[] = [
       edit: true
     }
 ]
+
+
+const defaultAdminRole: RolePerm = {
+  role: "admin",
+  view: true,
+  delete: true,
+  edit: true
+}
+
+
+export const getPermissions = (role:string, page: string): RolePerm => {
+
+  if (role == "admin") return defaultAdminRole
+
+
+  if (role === "pharmacy" && page === "medicine")
+        return medicinePerms.find((p:RolePerm)=> p.role === role) as RolePerm;
+
+ return {
+  role: role,
+  view: false,
+  delete: false,
+  edit: false
+ }
+}
