d09939f34359176c58260f48e7b2c740386b4599
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-05-09 06:55:50 +0200

    Add more nav icons

--- dashboard/src/components/dashboard/layout/nav-icons.tsx
+++ dashboard/src/components/dashboard/layout/nav-icons.tsx
@@ -2,23 +2,24 @@ import type { Icon } from '@phosphor-icons/react/dist/lib/types';
 import { ChartPie as ChartPieIcon } from '@phosphor-icons/react/dist/ssr/ChartPie';
 import { GearSix as GearSixIcon } from '@phosphor-icons/react/dist/ssr/GearSix';
 import { PlugsConnected as PlugsConnectedIcon } from '@phosphor-icons/react/dist/ssr/PlugsConnected';
-import { User as UserIcon } from '@phosphor-icons/react/dist/ssr/User';
+import { User, User as UserIcon } from '@phosphor-icons/react/dist/ssr/User';
 import { Users as UsersIcon } from '@phosphor-icons/react/dist/ssr/Users';
 import { Money as MoneyIcon } from '@phosphor-icons/react/dist/ssr/Money';
 import { ShoppingBag as ShoppingBagIcon } from '@phosphor-icons/react/dist/ssr/ShoppingBag';
 import { Pill as PillIcon } from '@phosphor-icons/react/dist/ssr/Pill';
 import { XSquare } from '@phosphor-icons/react/dist/ssr/XSquare';
-import { Scroll as ScrollIcon } from '@phosphor-icons/react/dist/ssr';
+import { FirstAid, Prescription } from '@phosphor-icons/react/dist/ssr';
 
 export const navIcons = {
   'chart-pie': ChartPieIcon,
   'gear-six': GearSixIcon,
   'plugs-connected': PlugsConnectedIcon,
   'shopping-bag': ShoppingBagIcon,
-  'scroll': ScrollIcon,
+  'pescription': Prescription,
+  'pharmacy': FirstAid,
   'money': MoneyIcon,
   'pill': PillIcon,
   'x-square': XSquare,
-  user: UserIcon,
+  'user': UserIcon,
   users: UsersIcon,
 } as Record<string, Icon>;
