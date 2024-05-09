592b42d87afdb772cae72fedb09bd8fc09547b2f
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-05-09 08:47:36 +0200

    Import  and use cards

--- dashboard/src/app/dashboard/page.tsx
+++ dashboard/src/app/dashboard/page.tsx
@@ -12,6 +12,10 @@ import { TasksProgress } from '@/components/dashboard/overview/tasks-progress';
 import { TotalCustomers } from '@/components/dashboard/overview/total-customers';
 import { TotalProfit } from '@/components/dashboard/overview/total-profit';
 import { Traffic } from '@/components/dashboard/overview/traffic';
+import { Pharmacies } from '@/components/dashboard/overview/pharmacies';
+import { TotalUsers } from '@/components/dashboard/overview/total-users';
+import { TotalApplications } from '@/components/dashboard/overview/total-applications';
+import { TotalOrders } from '@/components/dashboard/overview/total-orders';
 
 export const metadata = { title: `Overview | Dashboard | ${config.site.name}` } satisfies Metadata;
 
@@ -19,16 +23,16 @@ export default function Page(): React.JSX.Element {
   return (
     <Grid container spacing={3}>
       <Grid lg={3} sm={6} xs={12}>
-        <Budget diff={12} trend="up" sx={{ height: '100%' }} value="$24k" />
+        <Pharmacies diff={12} trend="up" sx={{ height: '100%' }} value="$24k" />
       </Grid>
       <Grid lg={3} sm={6} xs={12}>
-        <TotalCustomers diff={16} trend="down" sx={{ height: '100%' }} value="1.6k" />
+        <TotalUsers diff={16} trend="down" sx={{ height: '100%' }} value="1.6k" />
       </Grid>
       <Grid lg={3} sm={6} xs={12}>
-        <TasksProgress sx={{ height: '100%' }} value={75.5} />
+        <TotalApplications sx={{ height: '100%' }} value={75.5} />
       </Grid>
       <Grid lg={3} sm={6} xs={12}>
-        <TotalProfit sx={{ height: '100%' }} value="$15k" />
+        <TotalOrders sx={{ height: '100%' }} value="$15k" />
       </Grid>
       <Grid lg={8} xs={12}>
         <Sales
