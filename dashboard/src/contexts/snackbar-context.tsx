1ef6e1a2cf212fc3f8f6fa25bb22760fcafe08e8
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-05-11 00:18:55 +0200

    Change types

--- dashboard/src/contexts/snackbar-context.tsx
+++ dashboard/src/contexts/snackbar-context.tsx
@@ -1,61 +1,51 @@
 'use client';
 
 import * as React from 'react';
-
-import type { User } from '@/types/user';
-import { authClient } from '@/lib/auth/client';
-import { logger } from '@/lib/default-logger';
+;
 import GeneralSnackbar from '@/components/general/snackbar';
 
-export interface UserContextValue {
-  user: User | null;
-  error: string | null;
-  isLoading: boolean;
-  checkSession?: () => Promise<void>;
+export interface SnackbarContextValue {
+  open: boolean,
+  updateMessage: Function
 }
 
-export const UserContext = React.createContext<UserContextValue | undefined>(undefined);
+interface MessageOptions {
+  type: string;
+  title: string;
+  body:string;
+}
+
+export const SnackbarContext = React.createContext<SnackbarContextValue>({
+  open: false,
+  updateMessage: () => {}
+});
 
 export interface SnackbarProviderProps {
   children: React.ReactNode;
 }
 
 export function SnackbarProvider({ children }: SnackbarProviderProps): React.JSX.Element {
-  const [state, setState] = React.useState(false)
-  const [message, messageOptions] = React.useState<MessageOptions>({})
 
-
-
-  const [snackOptions, setOpenSnack] = React.useState<SnackbarProps>({
-    open: false,
-    setOpen: () => {},
-    title: "",
-    type: "",
-    body: ""
+  const [message, setMessageOptions] = React.useState<MessageOptions>({
+    type: '',
+    title: '',
+    body: ''
   })
+
   const [open, setOpen] = React.useState(false)
 
   const updateMessage = (options:MessageOptions) => {
     setOpen(true)
-    setOpenSnack({
-      ...options,
-      open: open,
-      setOpen: setOpen
-    })
+    setMessageOptions(options)
   }
 
 
-  React.useEffect(() => {
-    checkSession().catch((err) => {
-      logger.error(err);
-      // noop
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps -- Expected
-  }, []);
 
-  return <UserContext.Provider value={{ ...state, checkSession }}>
-    <GeneralSnackbar />
-    {children}</UserContext.Provider>;
+  return <SnackbarContext.Provider value={{ open, updateMessage }}>
+    <GeneralSnackbar open={open} setOpen={setOpen} snackOptions={message}/>
+    {children}</SnackbarContext.Provider>;
 }
 
-export const UserConsumer = UserContext.Consumer;
+export const useSnackbar = () => {
+  return React.useContext(SnackbarContext)
+}
