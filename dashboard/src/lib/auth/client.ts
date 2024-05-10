9217776439a1078d0fe68cbfdf3b0bb2ceb478a3
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-05-11 00:56:35 +0200

    Fix regsitraton bug

--- dashboard/src/lib/auth/client.ts
+++ dashboard/src/lib/auth/client.ts
@@ -2,6 +2,8 @@
 
 import { setCookie, deleteCookie, getCookie } from 'cookies-next';
 import type { User } from '@/types/user.type';
+import frontendClient from '@/services/frontend-client';
+import { APIResponse } from '@/types/api-response';
 
 
 function generateToken(): string {
@@ -33,14 +35,19 @@ export interface ResetPasswordParams {
 }
 
 class AuthClient {
-  async signUp(_: SignUpParams): Promise<{ error?: string }> {
+  async signUp(params: SignUpParams): Promise<APIResponse> {
     // Make API request
 
-    // We do not handle the API, so we'll just generate a token and store it in localStorage.
-    const token = generateToken();
-    setCookie('custom-auth-token', token);
+    let response = await frontendClient('post', 'user/register', {
+      ...params,
+      name: params.fullName
+    })
+    await this.signInWithPassword({email: params.email, password: params.password})
 
-    return {};
+    // We do not handle the API, so we'll just generate a token and store it in localStorage.
+
+
+    return response;
   }
 
   async signInWithOAuth(_: SignInWithOAuthParams): Promise<{ error?: string }> {
