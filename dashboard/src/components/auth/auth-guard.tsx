f26594ef4ccad5ae67b8429d755a8c1929a6c099
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-05-13 00:41:18 +0200

    User in pending should never see dashbaord

--- dashboard/src/components/auth/auth-guard.tsx
+++ dashboard/src/components/auth/auth-guard.tsx
@@ -17,7 +17,9 @@ export function AuthGuard({ children }: AuthGuardProps): React.JSX.Element | nul
   const { user, error, isLoading } = useUser();
   const [isChecking, setIsChecking] = React.useState<boolean>(true);
 
+
   const checkPermissions = async (): Promise<void> => {
+
     if (isLoading) {
       return;
     }
@@ -30,6 +32,26 @@ export function AuthGuard({ children }: AuthGuardProps): React.JSX.Element | nul
     if (!user) {
       logger.debug('[AuthGuard]: User is not logged in, redirecting to sign in');
       router.replace(paths.auth.signIn);
+      return;
+    }
+
+    if (user) {
+      logger.debug('[GuestGuard]: User is logged in, redirecting to dashboard');
+
+      if (user.role === "pharmacy"){
+        if (!user.pharmacy){
+          router.replace(paths.auth.registerPharmacy)
+        }else if (user.pharmacy && user.pharmacy.applicationStatus === "pending"){
+          router.replace(paths.registration.pending)
+        }else if (user.pharmacy && user.pharmacy.applicationStatus === "declined"){
+          router.replace(paths.registration.declined)
+        }else if (user.pharmacy && user.pharmacy.applicationStatus === "approved"){
+          router.replace(paths.dashboard.overview)
+        }
+      }
+
+      if (user.role === "admin")
+        router.replace(paths.dashboard.overview);
       return;
     }
 
