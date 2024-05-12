fa4015ea04acf6d21090ee940b3f58b05867f299
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-05-13 00:50:11 +0200

    use check user permissions

--- dashboard/src/components/auth/guest-guard.tsx
+++ dashboard/src/components/auth/guest-guard.tsx
@@ -4,10 +4,11 @@ import * as React from 'react';
 import { useRouter } from 'next/navigation';
 import Alert from '@mui/material/Alert';
 
-import { paths } from '@/paths';
+
 import { logger } from '@/lib/default-logger';
 import { useUser } from '@/hooks/use-user';
-import { useSnackbar } from '@/contexts/snackbar-context';
+import { checkUserPermissions } from '@/lib/auth/auth.utils';
+import { User } from '@/types/user.type';
 
 export interface GuestGuardProps {
   children: React.ReactNode;
@@ -18,7 +19,6 @@ export function GuestGuard({ children }: GuestGuardProps): React.JSX.Element | n
   const { user, error, isLoading } = useUser();
   const [isChecking, setIsChecking] = React.useState<boolean>(true);
 
-  const {updateMessage } = useSnackbar()
 
   const checkPermissions = async (): Promise<void> => {
     if (isLoading) {
@@ -30,25 +30,7 @@ export function GuestGuard({ children }: GuestGuardProps): React.JSX.Element | n
       return;
     }
 
-    if (user) {
-      logger.debug('[GuestGuard]: User is logged in, redirecting to dashboard');
-
-      if (user.role === "pharmacy"){
-        if (!user.pharmacy){
-          router.replace(paths.auth.registerPharmacy)
-        }else if (user.pharmacy && user.pharmacy.applicationStatus === "pending"){
-          router.replace(paths.registration.pending)
-        }else if (user.pharmacy && user.pharmacy.applicationStatus === "declined"){
-          router.replace(paths.registration.declined)
-        }else if (user.pharmacy && user.pharmacy.applicationStatus === "approved"){
-          router.replace(paths.dashboard.overview)
-        }
-      }
-
-      if (user.role === "admin")
-        router.replace(paths.dashboard.overview);
-      return;
-    }
+    checkUserPermissions(user as User, router)
 
     setIsChecking(false);
   };
