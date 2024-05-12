11f3f83e5234ba53877f2189bdeefe0149953fc0
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-05-12 22:36:37 +0200

    Create supbase util functions

--- dashboard/src/lib/supabase/frontend-client.tsx
+++ dashboard/src/lib/supabase/frontend-client.tsx
@@ -1,6 +1,6 @@
 
 import { User } from '@/types/user.type';
-import { createClient } from '@supabase/supabase-js'
+import { createClient, SupabaseClient } from '@supabase/supabase-js'
 import { getCookie } from 'cookies-next';
 
 // Create a single supabase client for interacting with your database
@@ -9,30 +9,6 @@ const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL as string, pr
 
 
 
-export const uploadFileToSupabase = async (file:any, bucket:string) => {
 
-
-     let userString:string | undefined = getCookie('custom-auth-user')
-
-     let user:User = JSON.parse(userString as string)
-
-
-
-
-  const { data:uploadData, error:uploadError } = await supabase.storage.from('logos').upload(`${user?._id}-${file.name}`, file, {
-    cacheControl: '3600',
-    upsert: false,
-  });
-
-  // @ts-ignore
-  let fileKey = uploadData["path"] as string
-
-  const { data:signedUrl, error:signedKeyError } = await supabase.storage
-                        .from('logos')
-                        .createSignedUrl(fileKey, 60 * 60 * 24 * 366 * 10)
-
-
-  return signedUrl?.signedUrl
-};
 
 export default supabase
---
+++ dashboard/src/lib/supabase/subapase.utils.ts
@@ -0,0 +1,32 @@
+import { User } from "@/types/user.type";
+import { SupabaseClient } from "@supabase/supabase-js";
+import { getCookie } from "cookies-next";
+
+
+
+
+export const uploadFileToSupabase = async (subaseClient: SupabaseClient, file:any, bucket:string) => {
+
+
+  let userString:string | undefined = getCookie('custom-auth-user')
+
+  let user:User = JSON.parse(userString as string)
+
+
+
+
+const { data:uploadData, error:uploadError } = await subaseClient.storage.from(bucket).upload(`${user?._id}-${file.name}`, file, {
+ cacheControl: '3600',
+ upsert: false,
+});
+
+// @ts-ignore
+let fileKey = uploadData["path"] as string
+
+const { data:signedUrl, error:signedKeyError } = await subaseClient.storage
+                     .from(bucket)
+                     .createSignedUrl(fileKey, 60 * 60 * 24 * 366 * 10)
+
+
+return signedUrl?.signedUrl
+};
