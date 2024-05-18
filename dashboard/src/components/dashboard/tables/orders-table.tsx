09f86867dd4284888dfccd1df86431f6dd804e6a
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-05-18 21:24:54 +0200

    Use the refcatored order

--- dashboard/src/components/dashboard/tables/orders-table.tsx
+++ dashboard/src/components/dashboard/tables/orders-table.tsx
@@ -21,34 +21,7 @@ import dayjs from 'dayjs';
 
 import { Permissions, RolePerm } from '@/types/permissions.type';
 import { useSelection } from '@/hooks/use-selection';
-import { User } from '@/types/user.type';
-import { Pharmacy } from '@/types/pharmacy.type';
-
-function noop(): void {
-  // do nothing
-}
-
-export interface Order {
-  _id: string;
-  userId: User;
-  pharmacyId: Pharmacy;
-  items: Array<any>; // You may want to specify the type of items if you have a specific structure for them
-  prescriptions: Array<any>; // You may want to specify the type of prescriptions if you have a specific structure for them
-  totalAmount: number;
-  status: string;
-  shippingAddress: {
-    // Define the structure of the shipping address object
-    // You can add more fields as needed
-    // Example:
-    streetAddress: string;
-    city: string;
-    state: string;
-    postalCode: string;
-  };
-  paymentMethod: string;
-  transactionId: string;
-  orderDate: string; // Assuming the date is represented as a string
-};
+import { Order } from '@/types/order.type';
 
 
 interface GeneralTableProps {
