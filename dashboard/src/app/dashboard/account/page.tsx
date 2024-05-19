fb7824eb2f92ce1d5ce78c20a310a14432e93302
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-05-19 21:48:44 +0200

    Add account page

--- dashboard/src/app/dashboard/account/page.tsx
+++ dashboard/src/app/dashboard/account/page.tsx
@@ -7,10 +7,19 @@ import Grid from '@mui/material/Unstable_Grid2';
 import { config } from '@/config';
 import { AccountDetailsForm } from '@/components/dashboard/account/account-details-form';
 import { AccountInfo } from '@/components/dashboard/account/account-info';
+import { cookies } from 'next/headers';
+import { User } from '@/types/user.type';
 
 export const metadata = { title: `Account | Dashboard | ${config.site.name}` } satisfies Metadata;
 
 export default function Page(): React.JSX.Element {
+
+
+
+  let userObject = cookies().get("custom-auth-user")
+  let user: User = JSON.parse(userObject?.value as string)
+
+
   return (
     <Stack spacing={3}>
       <div>
@@ -18,10 +27,10 @@ export default function Page(): React.JSX.Element {
       </div>
       <Grid container spacing={3}>
         <Grid lg={4} md={6} xs={12}>
-          <AccountInfo />
+          <AccountInfo user={user} />
         </Grid>
         <Grid lg={8} md={6} xs={12}>
-          <AccountDetailsForm />
+          <AccountDetailsForm user={user} />
         </Grid>
       </Grid>
     </Stack>
