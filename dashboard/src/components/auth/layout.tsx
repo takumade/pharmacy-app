4bdec35eaa90601b96bb4a4c49774b3fea8a1092
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-06-07 23:46:59 +0200

    Chnage image width

--- dashboard/src/components/auth/layout.tsx
+++ dashboard/src/components/auth/layout.tsx
@@ -24,7 +24,7 @@ export function Layout({ children }: LayoutProps): React.JSX.Element {
       <Box sx={{ display: 'flex', flex: '1 1 auto', flexDirection: 'column' }}>
         <Box sx={{ p: 3 }}>
           <Box component={RouterLink} href={paths.home} sx={{ display: 'inline-block', fontSize: 0 }}>
-            <DynamicLogo colorDark="light" colorLight="dark" height={32} width={122} />
+            <DynamicLogo colorDark="light" colorLight="dark" height={44} width={122} />
           </Box>
         </Box>
         <Box sx={{ alignItems: 'center', display: 'flex', flex: '1 1 auto', justifyContent: 'center', p: 3 }}>
