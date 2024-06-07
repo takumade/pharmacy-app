002b23c1476c57b313ec8493eb088acb703b3c17
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-06-07 23:47:06 +0200

    Change logo poath

--- dashboard/src/components/core/logo.tsx
+++ dashboard/src/components/core/logo.tsx
@@ -24,7 +24,7 @@ export function Logo({ color = 'dark', emblem, height = HEIGHT, width = WIDTH }:
   if (emblem) {
     url = color === 'light' ? '/assets/logo-emblem.svg' : '/assets/logo-emblem--dark.svg';
   } else {
-    url = color === 'light' ? '/assets/logo.svg' : '/assets/logo--dark.svg';
+    url = color === 'light' ? '/assets/logo.png' : '/assets/logo--dark.png';
   }
 
   return <Box alt="logo" component="img" height={height} src={url} width={width} />;
